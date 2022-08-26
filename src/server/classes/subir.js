19e793b6e721434a5b71c29b4143c0b4274871b5
Gerardo Bautista Castañeda <gerardo.bautistac@elektra.com.mx>
2022-08-26 18:22:25 -0500

    a

--- src/server/classes/subir.js
+++ src/server/classes/subir.js
@@ -95,16 +95,7 @@ class Subir {
                 // Create a child reference
             const fotografiasRef = ref(storage, 'fotografias/' + nombreFoto);
                   uploadString(fotografiasRef, base64Data, 'base64').then((snapshot) => {
-                    usuarioDB.foto = nombreFoto;
-            usuarioDB.save((err, userUpdate) => {
-
-                
-                if (err) return reject({ msg: 'Error db', err, code: 500 });
-                if (!userUpdate) return reject({ msg: 'No se pudo actualizar los datos.', code: 400 });
-                resolve(userUpdate);
-            });
-                  }).catch(err => console.log(err)).finally(e => {
-                    console.log(e)
+                   
                   })
             })
                 .catch(err => console.log('Err ' + err));
