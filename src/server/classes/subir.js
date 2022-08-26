51764451ae2077506255f9f1ea49b1f283b99bd6
Gerardo Bautista Castañeda <gerardo.bautistac@elektra.com.mx>
2022-08-26 18:31:57 -0500

    Delete

--- src/server/classes/subir.js
+++ src/server/classes/subir.js
@@ -9,7 +9,7 @@ const { response500, response400, response200, response201 } = require('../utils
 const { obtenerRutaDeCargaArchivos } = require('../utils/utils');
 
 const { initializeApp } = require("firebase/app");
-const { getStorage, ref, uploadBytes, uploadString } = require("firebase/storage");
+const { getStorage, ref, uploadBytes, uploadString, deleteObject } = require("firebase/storage");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -94,6 +94,11 @@ class Subir {
                 const base64Data = data.toString('base64');
                 // Create a child reference
             const fotografiasRef = ref(storage, 'fotografias/' + nombreFoto);
+            deleteObject(desertRef).then(() => {
+                console.log("File deleted successfully")
+              }).catch((error) => {
+                console.log("Uh-oh, an error occurred!")
+              });
                   uploadString(fotografiasRef, base64Data, 'base64').then((snapshot) => {
                     usuarioDB.foto = nombreFoto;
                     
