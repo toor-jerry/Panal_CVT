ff9b8dfc1c6739e12f66fa3ef0787aa0a69c96c4
Gerardo Bautista Castañeda <gerardo.bautistac@elektra.com.mx>
2022-08-27 20:07:44 -0500

    generar img

--- src/server/classes/subir.js
+++ src/server/classes/subir.js
@@ -61,6 +61,7 @@ class Subir {
                         .then((data) => {
                             const base64Data = data.toString('base64');
                             // Create a child reference
+                            console.log('imagen creada')
                             const fotografiasRef = ref(storage, 'fotografias/' + nombreFoto);
                             deleteObject(fotografiasRef).then(() => {
                                 console.log("File deleted successfully")
