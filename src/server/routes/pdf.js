049ec28f19870aaf578b03b7944669964e741aa5
Gerardo Bautista Castañeda <gerarchicharo37@gmail.com>
2022-08-31 00:09:32 -0500

    ssss

--- src/server/routes/pdf.js
+++ src/server/routes/pdf.js
@@ -13,25 +13,29 @@ app.get('/', checkSession, async(req, res) => {
 
     let usuario;
     var pathPdf;
-    usuario =  req.session.usuario._id;
-    return generatePDFServer(res, usuario)
     if (req.query.idUsuario) {
         usuario = req.query.idUsuario;
         await getBytes(ref(storage, 'cv/Custom_' + usuario + '.pdf')).then((response) => {
+            if (response) {
             res.setHeader('Content-Type', 'application/pdf')
             res.setHeader('Content-Disposition', 'attachment; filename=CV.Pdf')
             return res.send(Buffer.from(response))
+            } else {
+                return response404('file not found')
+            }
     }).catch((error) => {
         console.log("No encontrado");
-        
+        generatePDFServer(res, usuario)
     })
     }
         else {
             usuario =  req.session.usuario._id;
             await getBytes(ref(storage, 'cv/Custom_' + usuario + '.pdf')).then((response) => {
+                if (response) {
                 res.setHeader('Content-Type', 'application/pdf')
                 res.setHeader('Content-Disposition', 'attachment; filename=CV.Pdf')
                 return res.send(Buffer.from(response))
+                }
         }).catch((error) => {
             console.log("no encontrado")
             generatePDFServer(res, usuario)
@@ -47,6 +51,8 @@ function generatePDFServer(res, usuario) {
             console.log("Limpieza, eliminado el archivo de temp: " + nameFile)
         if (fs.existsSync(pathPdf)) {
             fs.unlinkSync(pathPdf);
+        } else {
+            return response404('File not found');
         }
         })
     }).catch(err => {
