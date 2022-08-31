3b945342264eb370542bdb392ce5ec0da40956ae
Gerardo Bautista Castañeda <gerarchicharo37@gmail.com>
2022-08-30 23:57:32 -0500

    adasss

--- src/server/routes/pdf.js
+++ src/server/routes/pdf.js
@@ -51,7 +51,7 @@ function generatePDFServer(res, usuario) {
         })
     }).catch(err => {
         console.log("No se pudo generar")
-        return response400(err)
+        return response404(err)
     })
 }
 
